6d8fce8459a586fdbaeb8677236c6d8425938e5f
Nitish280 <nsrccs@gmail.com>
2017-06-07 16:17:59 +0530

    grade

---
+++ src/Xpattern.java
@@ -0,0 +1,20 @@
+
+public class Xpattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, j;
+
+		for (i = 1; i <= 5; i++) {
+			for (j = 1; j <= 5; j++) {
+				if (j == i || j == 6 - i) {
+					System.out.print("x");
+					} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+}
